d4bd91dafc3a291a176c35463aad6cdb15e858de
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-01-25 13:48:45 +0200

    cf problem 69A

---
+++ problemSolving/problemSolving/A69.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class A69
+{
+    public static void Main()
+    {
+        //https://codeforces.com/contest/69/problem/A
+        int n = int.Parse(Console.ReadLine()), cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        while (n-- != 0)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            cnt1 += int.Parse(input[0]);
+            cnt2 += int.Parse(input[1]);
+            cnt3 += int.Parse(input[2]);
+        }
+        Console.WriteLine((cnt1 == 0 && cnt2 == 0 && cnt3 == 0) ? "YES" : "NO");
+    }
+}
