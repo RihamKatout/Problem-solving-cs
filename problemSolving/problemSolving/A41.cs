cbb4356278f53d765662512df5cb7ea406e0d746
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-01-25 13:45:43 +0200

    cf problem 41A

---
+++ problemSolving/problemSolving/A41.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class A41
+{
+    public static void Main()
+    {
+        //https://codeforces.com/contest/41/problem/A
+        string input1 = Console.ReadLine(), input2 = Console.ReadLine();
+        input2 = new string(input2.Select(c => c).Reverse().ToArray());
+        Console.WriteLine(input1.Equals(input2) ? "YES" : "NO");
+    }
+}
