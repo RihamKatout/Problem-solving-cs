ff6a6388f1287c8c8e4e59d1c4c7d6904421974c
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-01-27 23:55:20 +0200

    Task 11: second 5 problems on LeetCode

---
+++ problemSolving/problemSolving/Leetcode27.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    //https://leetcode.com/problems/remove-element/description/
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0, last = nums.Length - 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+            {
+                k++;
+                nums[i] = nums[last];
+                nums[last] = -1;
+                last--;
+                i--;
+            }
+        }
+        return nums.Length - k;
+    }
+}
---
+++ problemSolving/problemSolving/Leetcode83.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        int last = 101, flag = 0;
+        if (head == null) return null;
+        ListNode ans = new();
+        ListNode tmp = new();
+
+        while (head != null)
+        {
+            if (last != head.val)
+            {
+                if (flag == 0)
+                {
+                    ans.val = head.val;
+                    tmp = ans;
+                    flag = 1;
+                }
+                else
+                {
+                    tmp.next = new(head.val);
+                    tmp = tmp.next;
+                }
+                last = head.val;
+            }
+            head = head.next;
+        }
+        return ans;
+    }
+}
