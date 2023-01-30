d193a50df00a3788f45ef2f2450c7c5b0842710b
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-01-30 22:21:57 +0200

    problem 31A codeforces

---
+++ problemSolving/problemSolving/A31.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemSolving
+{
+    internal class A31
+    {
+    }
+}
